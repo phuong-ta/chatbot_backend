940bb2810509508cd43c97a5ed8df02cf8e5c508
Hoàng Phương Tạ <phuong.ta011290@gmail.com>
2024-12-04 19:23:52 +0200

    index html created!

--- main.py
+++ main.py
@@ -1,25 +1,9 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
 
-
+from routers.index import index_router
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
-templates = Jinja2Templates(directory="templates")
-
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/items/{id}", response_class=HTMLResponse)
-async def read_item(request: Request, id: str):
-    return templates.TemplateResponse(
-        request=request, name="item.html", context={"id": id}
-    )
+app.include_router(index_router)
---
+++ routers/index.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+index_router = APIRouter()
+
+templates = Jinja2Templates(directory="templates")
+
+
+@index_router.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="index.html", context={"data_files": "ok"}
+    )
+
+
+@index_router.get("/list_files")
+def list_files():
+    fruit_list = ["apple", "banana", "cherry"]
+    return {"data_files": fruit_list}
+
+
+@index_router.post("/upload_file")
+async def upload_file():
+    return {"message": "Admin getting schwifty"}
