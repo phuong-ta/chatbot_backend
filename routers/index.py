5fcbfe55c2c7af8ac837fed162d3cf32a2dd8f7d
Hoàng Phương Tạ <phuong.ta011290@gmail.com>
2024-12-11 13:07:53 +0200

    add file ok

--- routers/index.py
+++ routers/index.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter, Request
+from typing import Annotated
+
+from fastapi import APIRouter, Request, status, UploadFile, Form, HTTPException
 from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
 from fastapi.responses import JSONResponse
+from fastapi.templating import Jinja2Templates
 
 index_router = APIRouter()
 
@@ -21,6 +23,19 @@ def list_files():
     return JSONResponse(content={"data_files": fruit_list})
 
 
-@index_router.post("/upload_file")
-async def upload_file():
-    return {"message": "Admin getting schwifty"}
+@index_router.post("/upload_file", status_code=status.HTTP_201_CREATED)
+async def create_upload_file(description: Annotated[str, Form()], password: Annotated[str, Form()], file: UploadFile):
+    # Process the file, description, and password as needed
+    # Check the password
+    if password != "metropolia_pepe":
+        # Raise an HTTP exception with a 403 status code
+        raise HTTPException(status_code=403, detail="Wrong password")
+
+    # If the password is correct, return the file information
+    return {
+        "success": True,
+        "message": "File uploaded successfully",
+        "filename": file.filename,
+        "description": description,
+    }
+
