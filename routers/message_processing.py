fee53b053dc0e6a5ba2a0ad72f6413b9bb55f077
Hoàng Phương Tạ <phuong.ta011290@gmail.com>
2024-12-11 15:11:40 +0200

    modified message

--- routers/message_processing.py
+++ routers/message_processing.py
@@ -1,10 +1,17 @@
 from typing import Annotated
 
-from fastapi import APIRouter, status, Form
+from fastapi import APIRouter, status, Form, HTTPException
 
 message_router = APIRouter()
 
 
 @message_router.post("/message/", status_code=status.HTTP_201_CREATED)
-async def create_item(message: Annotated[str, Form()]):
-    return {"response": message + " server"}
+async def create_upload_file(message: Annotated[str, Form()]):
+    # Process the file, description, and password as needed
+    # Check the password
+    if message == "":
+        # Raise an HTTP exception with a 403 status code
+        raise HTTPException(status_code=403, detail="Wrong password")
+
+    # If the password is correct, return the file information
+    return {"response": message + " from backend"}
