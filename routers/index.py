c296f44aa0a6854817f2fbd2a16c59b8365abbab
Hoàng Phương Tạ <phuong.ta011290@gmail.com>
2024-12-13 08:56:32 +0200

    test list file again!

--- routers/index.py
+++ routers/index.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.templating import Jinja2Templates
+from google.cloud import storage
 
 index_router = APIRouter()
-
 
 templates = Jinja2Templates(directory="templates")
 
@@ -15,7 +15,19 @@ async def root(request: Request):
     )
 
 
+def list_file(bucket_name="chatbot-data-metropolia"):
+    file_list = []
+    storage_client = storage.Client()
+
+    # Note: Client.list_blobs requires at least package version 1.17.0.
+    blobs = storage_client.list_blobs(bucket_name)
+
+    # Note: The call returns a response only when the iterator is consumed.
+    for blob in blobs:
+        file_list.append(blob.name)
+    return file_list
+
+
 @index_router.get("/list_files")
 def list_files():
-    fruit_list = ["apple", "banana", "cherry", "juice"]
-    return JSONResponse(content={"data_files": fruit_list})
+    return JSONResponse(content={"data_files": list_file()})
