d5c6561a28a095159e5651ebba8571dc64c11963
Hoàng Phương Tạ <phuong.ta011290@gmail.com>
2024-12-16 11:25:29 +0200

    test open ai again

--- routers/message_processing.py
+++ routers/message_processing.py
@@ -1,7 +1,10 @@
 from typing import Annotated
 
 from fastapi import APIRouter, status, Form, HTTPException
+from langchain_openai import OpenAI
+import os
 
+openai_key = os.environ["OPENAI_KEY"]
 
 
 
@@ -12,9 +15,11 @@ message_router = APIRouter()
 async def create_upload_file(message: Annotated[str, Form()]):
     # Process the file, description, and password as needed
     # Check the password
+    llm = OpenAI()
     if message == "":
         # Raise an HTTP exception with a 403 status code
         raise HTTPException(status_code=403, detail="Wrong password")
 
     # If the password is correct, return the file information
-    return {"response": message + " from backend"}
+    #return {"response": message + " from backend"}
+    return {"response": llm.invoke(message)}
