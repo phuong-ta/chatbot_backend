0a31e3aba1850fddfd1cc53cb365f5863712937f
Hoàng Phương Tạ <phuong.ta011290@gmail.com>
2024-12-16 11:11:39 +0200

    test again

--- routers/message_processing.py
+++ routers/message_processing.py
@@ -1,22 +1,8 @@
-import getpass
-import os
-from http.client import responses
 from typing import Annotated
+
 from fastapi import APIRouter, status, Form, HTTPException
-from langchain_openai import ChatOpenAI, OpenAI
 
 
-api_key=os.environ['OPENAI_KEY']
-
-llm = OpenAI(
-    model="gpt-3.5-turbo-instruct",
-    temperature=0,
-    max_retries=2,
-    api_key=api_key,
-    # base_url="...",
-    # organization="...",
-    # other params...
-)
 
 
 message_router = APIRouter()
@@ -30,15 +16,5 @@ async def create_upload_file(message: Annotated[str, Form()]):
         # Raise an HTTP exception with a 403 status code
         raise HTTPException(status_code=403, detail="Wrong password")
 
-    response = llm.invoke(message)
     # If the password is correct, return the file information
-    return {"response": "Hi "+ response}
-
-    """
-    responses = llm.invoke(message)
-    if responses["status"] == "ok":
-        return {"response": llm.invoke(message)}
-    else:
-        return {"response": "Error"}
-        
-          """
+    return {"response": message + " from backend"}
