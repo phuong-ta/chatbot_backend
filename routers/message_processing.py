2d9388bd1f4e4fb6262e2fe6990e9f0fda66eabe
Hoàng Phương Tạ <phuong.ta011290@gmail.com>
2024-12-17 10:54:41 +0200

    test open ai again

--- routers/message_processing.py
+++ routers/message_processing.py
@@ -4,23 +4,22 @@ from fastapi import APIRouter, status, Form, HTTPException
 from langchain_openai import OpenAI
 from langchain_google_genai import GoogleGenerativeAI
 import os
+import sys
 
-api_key = "AIzaSyD0AbJ8GiYWiFCdYdFZSDDPa2jhWb7oc8E"
+
+
 
 message_router = APIRouter()
+api_key = os.environ.get("OPENAI_API_KEY")
 
 
 @message_router.post("/message/", status_code=status.HTTP_201_CREATED)
 async def create_upload_file(message: Annotated[str, Form()]):
-    # Process the file, description, and password as needed
-    # Check the password
-    # llm = OpenAI( api_key=api_key)
-    llm = GoogleGenerativeAI(model="gemini-1.5-flash", google_api_key=api_key)
+
+    #llm = GoogleGenerativeAI(model="gemini-1.5-flash", google_api_key=api_key)
+    llm = OpenAI(model_name="gpt-3.5-turbo-instruct", api_key=api_key)
     if message == "":
         # Raise an HTTP exception with a 403 status code
         raise HTTPException(status_code=403, detail="Wrong password")
-
-    # If the password is correct, return the file information
-    #return {"response": message + " from backend"}
 
     return {"response": llm.invoke(str(message))}
