5dd9d02ef69cdf8f34d161b16d7d2dd5f39e4d95
Hoàng Phương Tạ <phuong.ta011290@gmail.com>
2024-12-17 13:58:51 +0200

    try google ai again

--- routers/message_processing.py
+++ routers/message_processing.py
@@ -16,10 +16,15 @@ api_key = os.environ.get("GOOGLE_API_KEY")
 @message_router.post("/message/", status_code=status.HTTP_201_CREATED)
 async def create_upload_file(message: Annotated[str, Form()]):
 
-    #llm = GoogleGenerativeAI(model="gemini-1.5-flash", google_api_key=api_key)
-    llm = OpenAI(model_name="gpt-3.5-turbo-instruct", api_key=api_key)
+    llm = GoogleGenerativeAI(model="gemini-1.5-flash", google_api_key=api_key)
+    #llm = OpenAI(model_name="gpt-3.5-turbo-instruct", api_key=str(api_key))
     if message == "":
         # Raise an HTTP exception with a 403 status code
         raise HTTPException(status_code=403, detail="Wrong password")
 
-    return {"response": llm.invoke(str(message))}
+    response  = llm.invoke(str(message))
+
+    if response:
+        return {"response": response}
+    else:
+        return {"response": "error"}
