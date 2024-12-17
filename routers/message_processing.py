ae01598fe77b270cd0abb2de01fa236eadd95e02
Hoàng Phương Tạ <phuong.ta011290@gmail.com>
2024-12-17 09:32:36 +0200

    test with gemini

--- routers/message_processing.py
+++ routers/message_processing.py
@@ -5,8 +5,7 @@ from langchain_openai import OpenAI
 from langchain_google_genai import GoogleGenerativeAI
 import os
 
-api_key = os.environ["GEMINI_KEY"]
-
+api_key = "AIzaSyD0AbJ8GiYWiFCdYdFZSDDPa2jhWb7oc8E"
 
 message_router = APIRouter()
 
@@ -16,11 +15,12 @@ async def create_upload_file(message: Annotated[str, Form()]):
     # Process the file, description, and password as needed
     # Check the password
     # llm = OpenAI( api_key=api_key)
-    llm = GoogleGenerativeAI(model="models/text-bison-001", google_api_key=api_key)
+    llm = GoogleGenerativeAI(model="gemini-1.5-flash", google_api_key=api_key)
     if message == "":
         # Raise an HTTP exception with a 403 status code
         raise HTTPException(status_code=403, detail="Wrong password")
 
     # If the password is correct, return the file information
     #return {"response": message + " from backend"}
-    return {"response": llm.invoke(message)}
+
+    return {"response": llm.invoke(str(message))}
