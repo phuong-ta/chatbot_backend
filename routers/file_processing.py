69ba3126a63deba10f82725fc60d51c160d474b6
Hoàng Phương Tạ <phuong.ta011290@gmail.com>
2024-12-18 13:20:51 +0200

    test vector store

--- routers/file_processing.py
+++ routers/file_processing.py
@@ -2,6 +2,12 @@ from typing import Annotated
 
 from fastapi import APIRouter, status, UploadFile, Form, HTTPException
 from google.cloud import storage
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain_openai import OpenAIEmbeddings
+
 
 file_router = APIRouter()
 
@@ -19,6 +25,19 @@ def upload_blob_from_memory(bucket_name, contents, destination_blob_name, metada
     return True
 
 
+def load_file_from_cloud(bucket_name, destination_blob_name):
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+    loader = PyPDFLoader(blob.download_as_string().decode("utf-8"))
+    docs = loader.load()
+    return docs
+
+
+
+
+
+
 @file_router.post("/upload_file", status_code=status.HTTP_201_CREATED)
 async def create_upload_file(description: Annotated[str, Form()], password: Annotated[str, Form()], file: UploadFile):
     # Process the file, description, and password as needed
@@ -27,21 +46,47 @@ async def create_upload_file(description: Annotated[str, Form()], password: Anno
         # Raise an HTTP exception with a 403 status code
         raise HTTPException(status_code=403, detail="Wrong password")
     contents = file.file.read()
+
     file.file.seek(0)
     # Add metadata (optional)
     metadata = {
         "name": file.filename,
         "description": description
     }
+
     bucket_name = "chatbot-data-metropolia"
     original_data_path = "original"
-    vector_data_path = "original"
+    vector_data_path = "vector"
 
+    # upload file to cloud
     upload_blob_from_memory(bucket_name=bucket_name, contents=contents,
                             destination_blob_name=f"{original_data_path}/{file.filename}", metadata=metadata)
-    # If the password is correct, return the file information
-    return {
-        "success": True,
-        "message": "File uploaded successfully",
-        "filename": file.filename
-    }
+
+    # load file  from cloud
+    documents = load_file_from_cloud(bucket_name=bucket_name, destination_blob_name=f"{original_data_path}/{file.filename}")
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+    texts = text_splitter.split_documents(documents=documents)
+
+    vectordb = Chroma.from_documents(
+        documents=texts,
+        embedding=OpenAIEmbeddings()
+    )
+
+    # upload file to cloud
+
+    upload_blob_from_memory(bucket_name=bucket_name, contents=vectordb,
+                            destination_blob_name=f"{vector_data_path}/{file.filename}")
+
+    if upload_blob_from_memory(bucket_name=bucket_name, contents=vectordb,
+                            destination_blob_name=f"{vector_data_path}/{file.filename}"):
+
+
+        # If the password is correct, return the file information
+        return {
+            "success": True,
+            "message": "File uploaded successfully",
+            "filename": file.filename
+        }
+
+    else:
+        return {}
