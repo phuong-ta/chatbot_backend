374b8ee3ef98fe35e09773be42e9bac12878d3c5
Hoàng Phương Tạ <phuong.ta011290@gmail.com>
2024-12-19 12:26:59 +0200

    create corpus

--- routers/file_processing.py
+++ routers/file_processing.py
@@ -22,7 +22,7 @@ def upload_blob_from_memory(bucket_name, contents, destination_blob_name, metada
     blob.upload_from_string(contents)
     return True
 
-"""
+
 def store_vector_data(project_id, location, display_name, bucket_name, file_paths,
                       embedding_model="text-embedding-004"):
     # Initialize Vertex AI API once per session
@@ -37,7 +37,7 @@ def store_vector_data(project_id, location, display_name, bucket_name, file_path
         display_name=display_name,
         embedding_model_config=embedding_model_config,
     )
-"""
+
 
 @file_router.post("/upload_file", status_code=status.HTTP_201_CREATED)
 async def create_upload_file(description: Annotated[str, Form()], password: Annotated[str, Form()], file: UploadFile):
@@ -59,7 +59,7 @@ async def create_upload_file(description: Annotated[str, Form()], password: Anno
     upload_blob_from_memory(bucket_name=bucket_name, contents=contents,
                             destination_blob_name=f"{original_data_path}/{file.filename}", metadata=metadata)
 
-    # store_vector_data(project_id=3046579594799874048,location="europe-north1",display_name="metropolia_rag",bucket_name="metropolia_chatobt", file_paths="gs://metropolia_chatobt",embedding_model="text-embedding-004")
+    store_vector_data(project_id=3046579594799874048,location="europe-north1",display_name="metropolia_rag",bucket_name="metropolia_chatobt", file_paths="gs://metropolia_chatobt",embedding_model="text-embedding-004")
 
     # If the password is correct, return the file information
     return {
