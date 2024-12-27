f782ef60c3677cd4726f57e812cc5ea43b51e87a
Hoàng Phương Tạ <phuong.ta011290@gmail.com>
2024-12-27 06:53:09 +0200

    test with website again

--- routers/web_processing.py
+++ routers/web_processing.py
@@ -3,11 +3,12 @@ import os
 from typing import Annotated
 
 import vertexai
-from bs4 import BeautifulSoup
+
 from fastapi import APIRouter, status, Form, HTTPException
 from vertexai.preview import rag
 
 web_router = APIRouter()
+
 api_key = os.environ.get("OPENAI_API_KEY")
 
 from google.cloud import storage
@@ -27,53 +28,6 @@ def get_json_from_bucket(bucket_name, file_name):
     return json_data
 
 
-""""
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = get_json_from_bucket(bucket_name="metropolia_chatobt",
-                                                                    file_name="google_credentials.json")
-                                                                    """
-
-
-def upload_blob_from_memory(bucket_name, contents, destination_blob_name, metadata=None):
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(destination_blob_name)
-
-    # Set metadata if provided
-    if metadata:
-        blob.metadata = metadata
-
-    blob.upload_from_string(contents)
-    return True
-
-
-def store_vector_data(project_id, location, corpus_name, file_name):
-    # Initialize Vertex AI API once per session
-    vertexai.init(project=project_id, location=location)
-    bucket_path = [f"gs://metropolia_chatobt/{file_name}"]
-
-    # Import Files to the RagCorpus
-    response = rag.import_files(
-        corpus_name=corpus_name,
-        paths=bucket_path,
-        chunk_size=512,  # Optional
-        chunk_overlap=100,  # Optional
-        max_embedding_requests_per_min=900,  # Optional
-    )
-
-    return response.imported_rag_files_count
-
-
-def get_html_file_content(web_link):
-    # Load and parse the HTML file
-    with open(web_link, "r", encoding="utf-8") as html_file:
-        soup = BeautifulSoup(html_file, "html.parser")
-    # Extract meaningful text
-    text_content = soup.get_text(separator="\n")  # Extracts text and separates it by lines
-
-    # Optional: Remove extra whitespaces
-    text_content = "\n".join([line.strip() for line in text_content.splitlines() if line.strip()])
-
-    return text_content
 
 
 @web_router.post("/upload_website", status_code=status.HTTP_201_CREATED)
@@ -84,11 +38,9 @@ async def create_upload_file(website: Annotated[str, Form()], web_name: Annotate
     if password != "metropolia_pepe":
         # Raise an HTTP exception with a 403 status code
         raise HTTPException(status_code=403, detail="Wrong password")
-    web_link = website
-    # Add metadata (optional)
 
     metadata = {
-        "name": web_link,
+        "name": web_name,
         "description": description
     }
     """
@@ -107,5 +59,5 @@ async def create_upload_file(website: Annotated[str, Form()], web_name: Annotate
     return {
         "success": True,
         "message": "File uploaded successfully",
-        "filename": web_link
+        "filename": web_name
     }
