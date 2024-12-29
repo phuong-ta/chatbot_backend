8dcf065b927516eb8e86d3017835a6b1c1df2c9e
Hoàng Phương Tạ <phuong.ta011290@gmail.com>
2024-12-29 05:17:23 +0200

    modify requirements file

--- routers/web_processing.py
+++ routers/web_processing.py
@@ -16,26 +16,30 @@ api_key = os.environ.get("OPENAI_API_KEY")
 
 from google.cloud import storage
 
-
+"""
 def process_url (url):
     response = requests.get(url)
     response.raise_for_status()  # Raise an exception for bad status codes
 
     soup = BeautifulSoup(response.content, "html.parser")
     text = soup.get_text()
-    upload_to_cloud_storage(text, url)
+    upload_blob_from_memory(text, url)
 
 
-def upload_to_cloud_storage(text, url):
-    """
-    Uploads text to Cloud Storage.
-    """
+def upload_blob_from_memory(bucket_name, contents, destination_blob_name, metadata=None):
 
-    bucket_name = "metropolia_chatobt"
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(f"{url.replace('/', '_')}.txt")
-    blob.upload_from_string(text)
+    blob = bucket.blob(destination_blob_name)
+
+    # Set metadata if provided
+    if metadata:
+        blob.metadata = metadata
+
+    blob.upload_from_string(contents)
+    return True
+
+"""
 
 @web_router.post("/upload_website", status_code=status.HTTP_201_CREATED)
 async def create_upload_file(website: Annotated[str, Form()], web_name: Annotated[str, Form()],
@@ -50,9 +54,9 @@ async def create_upload_file(website: Annotated[str, Form()], web_name: Annotate
         "name": web_name,
         "description": description
     }
-    """
+
     bucket_name = "metropolia_chatobt"
-    """
+
     # If the password is correct, return the file information
     return {
         "success": True,
